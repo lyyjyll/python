10fccc2dc383364221c0db980a8078c6f1da0296
IWorkstation <1477547082@qq.com>
2021-09-12 23:25:17 +0800

    add

--- 实战小练习/202109/20210903Requests库/0_obtain_nima_proxy.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-url = 'http://www.nimadaili.com/https/'
-
-for table in pd.read_html(url):
-
-    table.to_csv('nima_proxy.csv',mode='a',encoding='utf-8')
--- 实战小练习/202109/20210903Requests库/0_obtain_xila_proxy.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-url = 'http://www.xiladaili.com/https/'
-
-for table in pd.read_html(url):
-
-    table.to_csv('proxy.csv',mode='a',encoding='utf-8')
