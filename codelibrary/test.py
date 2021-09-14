b9c3805ee813e159d87582724ba7489420aa30c6
IWorkstation <1477547082@qq.com>
2021-09-14 17:43:31 +0800

    hello

---
+++ codelibrary/test.py
@@ -0,0 +1,19 @@
+from Office import DoExcel as de 
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame(np.arange(1,13).reshape(3,4))
+de.create_xlsx(df,'data')
+df.columns = ['帅','个','皮','hello']
+df.index = ['world','wor','yum']
+de.save2sheet(df,filename='Test')
+
+
+
+
+
+
+
+
+
+
